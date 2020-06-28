2a0feff77068ff1837f6ca3648c7e01d0e33f394
venkat541 <67378422+venkat541@users.noreply.github.com>
2020-06-28 10:01:49 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+/* This is a simple Java program. 
+   FileName : "HelloWorld.java". */
+class HelloWorld 
+{ 
+    // Your program begins with a call to main(). 
+    // Prints "Hello, World" to the terminal window. 
+    public static void main(String args[]) 
+    { 
+        System.out.println("Welcome helloworld"); 
+    } 
+}
+
+git log --format=%aD HelloWorld.java | tail -1
+
